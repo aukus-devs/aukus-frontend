5880afc314e233b5ae643d31f65a99b6b87d1160
Maxim Velesyuk <max.velesyuk@gmail.com>
2024-12-11 12:29:23 +0100

    Move theme creation into user context

--- src/context/UserProvider.tsx
+++ src/context/UserProvider.tsx
@@ -1,7 +1,14 @@
+import {
+  createTheme,
+  CssBaseline,
+  PaletteColor,
+  Theme,
+  ThemeProvider,
+} from '@mui/material'
 import { useQuery } from '@tanstack/react-query'
 import { createContext, useContext, useEffect, useState } from 'react'
 import { fetchCurrentUser } from 'utils/api'
-import { CurrentUser } from 'utils/types'
+import { Color, CurrentUser } from 'utils/types'
 
 const UserContext = createContext<CurrentUser | null>(null)
 
@@ -26,7 +33,76 @@ export function UserProvider({ children }: { children: React.ReactNode }) {
     }
   }, [currentUserData])
 
+  const buttonColor =
+    playerInfo?.name === 'UselessMouth' ? Color.pink : Color.blue
+
+  let darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: buttonColor,
+      },
+      secondary: {
+        main: '#007AFF',
+      },
+      info: {
+        main: Color.greyLight,
+      },
+      text: {
+        primary: '#fff',
+        secondary: '#a6d4fa',
+      },
+      background: {
+        default: '#0c0c0c',
+      },
+    },
+    typography: {
+      fontFamily: '"Golos Text", sans-serif',
+      fontWeightRegular: 600,
+    },
+    components: {
+      MuiButton: {
+        defaultProps: {
+          variant: 'contained',
+          color: 'primary',
+          disableElevation: true,
+          disableRipple: true,
+        },
+        styleOverrides: {
+          root: {
+            textTransform: 'none',
+            fontWeight: 600,
+          },
+        },
+      },
+    },
+  })
+
+  darkTheme = createTheme(darkTheme, {
+    palette: makeCustomColors(darkTheme, Color),
+  })
+
   return (
-    <UserContext.Provider value={playerInfo}>{children}</UserContext.Provider>
+    <UserContext.Provider value={playerInfo}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </UserContext.Provider>
   )
+}
+
+function makeCustomColors(theme: Theme, colors: { [key: string]: string }) {
+  const transformedColors: { [key: string]: PaletteColor } = {}
+
+  Object.entries(colors).forEach(([key, value]) => {
+    const customKey = `custom${key.charAt(0).toUpperCase()}${key.slice(1)}`
+
+    transformedColors[customKey] = theme.palette.augmentColor({
+      color: { main: value },
+      name: customKey,
+    })
+  })
+
+  return transformedColors
 }
