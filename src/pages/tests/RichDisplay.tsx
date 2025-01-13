d846be012aaa6e61e5aef2690f60f1622a7f5f67
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-01-13 21:25:00 +0100

    Fix types & build

--- src/pages/tests/RichDisplay.tsx
+++ src/pages/tests/RichDisplay.tsx
@@ -1,4 +1,4 @@
-import { SlateElement } from 'src/utils/declaration'
+import { SlateElement, SlateText } from 'src/types/declaration'
 
 type Props = {
   data: SlateElement[]
@@ -18,25 +18,64 @@ function RichElement({ element }: { element: SlateElement }) {
   const textAlign = element.align ?? 'left'
   switch (element.type) {
     case 'paragraph':
-      return <p style={{ textAlign }}>{element.children}</p>
+      return (
+        <p style={{ textAlign }}>
+          <RichElementText items={element.children} />
+        </p>
+      )
     case 'heading-one':
-      return <h1 style={{ textAlign }}>{element.children}</h1>
+      return (
+        <h1 style={{ textAlign }}>
+          <RichElementText items={element.children} />
+        </h1>
+      )
     case 'heading-two':
-      return <h2 style={{ textAlign }}>{element.children}</h2>
+      return (
+        <h2 style={{ textAlign }}>
+          <RichElementText items={element.children} />
+        </h2>
+      )
     case 'bulleted-list':
-      return <ul style={{ textAlign }}>{element.children}</ul>
+      return (
+        <ul style={{ textAlign }}>
+          <RichElementText items={element.children} />
+        </ul>
+      )
     case 'numbered-list':
-      return <ol style={{ textAlign }}>{element.children}</ol>
+      return (
+        <ol style={{ textAlign }}>
+          <RichElementText items={element.children} />
+        </ol>
+      )
     case 'list-item':
-      return <li style={{ textAlign }}>{element.children}</li>
+      return (
+        <li style={{ textAlign }}>
+          <RichElementText items={element.children} />
+        </li>
+      )
     case 'block-quote':
-      return <blockquote style={{ textAlign }}>{element.children}</blockquote>
+      return (
+        <blockquote style={{ textAlign }}>
+          <RichElementText items={element.children} />
+        </blockquote>
+      )
 
     default:
       return (
         <p style={{ color: 'red' }}>
-          Unsupported: {element.type}: {element.children}
+          Unsupported: {element.type}:{' '}
+          <RichElementText items={element.children} />
         </p>
       )
   }
+}
+
+function RichElementText({ items }: { items: SlateText[] }) {
+  return (
+    <>
+      {items.map((item: SlateText, index: number) => {
+        return <span key={index}>{item.text}</span>
+      })}
+    </>
+  )
 }
---
+++ src/types/declarations.d.ts
@@ -0,0 +1,18 @@
+declare module '@mapcar/dice-box' {
+  // Declare the DiceBox class or export as any
+  const DiceBox: any // Replace `any` with specific types if known
+  export default DiceBox
+}
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    'snow-effect': React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement>,
+      HTMLElement
+    > & {
+      color?: string
+      flakes?: number
+      speed?: number
+    }
+  }
+}
