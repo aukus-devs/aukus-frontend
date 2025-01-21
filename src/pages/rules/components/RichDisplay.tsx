c80599ad1ca4c7b0edc5525886ec6c0fca68829c
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-01-21 01:08:58 +0100

    Update rich display style

--- src/pages/rules/components/RichDisplay.tsx
+++ src/pages/rules/components/RichDisplay.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function RichDisplay({ data }: Props) {
   return (
-    <Box style={{ border: '1px solid white', width: '720px', padding: '20px' }}>
+    <Box style={{ width: '800px' }}>
       <RichChildren items={data} />
     </Box>
   )
